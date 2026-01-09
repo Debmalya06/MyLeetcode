7033156aaf33a787a73670194f2d7afcad2bc891
Debmalya Pan <134578586+Debmalya06@users.noreply.github.com>
2026-01-10 01:44:36 +0530

    Time: 0 ms (100%), Space: 43.4 MB (63.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,3 +1,26 @@
+//Override Approach
+// class Solution {
+//   public void sortColors(int[] arr) {
+//   int two=0,zero=0,one=0;
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]==0) zero++;
+//             else if(arr[i]==1) one++;
+//             else two++;
+//         }
+//         int index= 0;
+//         for(int i=0;i<zero;i++) {
+//             arr[index++]=0;
+//         }
+//         for(int i=0;i<one;i++) {
+//             arr[index++]=1;
+//         }
+//         for(int i=0;i<two;i++) {
+//             arr[index++]=2;
+//         }
+// }
+// }
+//Dutch National Flag Algo
+
 class Solution {
   public void sortColors(int[] nums) {
     int low = 0, mid = 0, high = nums.length - 1;
