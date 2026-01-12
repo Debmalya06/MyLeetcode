bbfca5aa6ee832de7db3133d00912f60e7e53f94
Debmalya Pan <134578586+Debmalya06@users.noreply.github.com>
2026-01-13 01:50:06 +0530

    Time: 54 ms (63.71%), Space: 70.5 MB (58.42%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,18 +1,31 @@
 class Solution {
-    public int totalFruit(int[] fruits) {
-        int low=0,high=0,ans =0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        while(high<fruits.length){
-            int n=fruits[high];
-            map.put(n, map.getOrDefault(n, 0) + 1);
-            while(map.size()>2){
-                int n2=fruits[low];
-                map.put(n2,map.get(n2)-1);
-                if(map.get(n2)==0) map.remove(n2);
-                low++;
+    public static int totalFruit(int[] fruits) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int i=0, j=0, ans=0;
+        
+        while(j<fruits.length){
+            int n = fruits[j];//calculation
+            map.put(n,map.getOrDefault(n,0)+1);
+
+              //logic but that will not works always under (map.size()==2))
+              //in case on {0}  it should picks  also, here that fail❌
+//            if(map.size()==2){
+//                ans= Math.max(j-i+1,ans);
+//            }
+
+          //window conditions adjust.
+            if(map.size()>2){
+                while(map.size()>2){
+                    int x=fruits[i];
+                    map.put(x, map.get(x) - 1);
+                    if(map.get(x)==0) {
+                        map.remove(x);
+                    }
+                    i++;
+                }
             }
-            ans = Math.max(ans,(high-low+1));
-            high++;
+            ans= Math.max(j-i+1,ans);//here is the logic that find ans.(for <2 it will works 🤞)
+            j++;
         }
         return ans;
     }
