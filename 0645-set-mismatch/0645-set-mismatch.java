4cc82488940f196bf84aa11bde41476019e5e276
Debmalya Pan <134578586+Debmalya06@users.noreply.github.com>
2026-01-22 13:14:01 +0530

    Time: 2 ms (94.01%), Space: 47.8 MB (31.54%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,26 +1,15 @@
 class Solution {
-    public int[]  findErrorNums(int[] nums) {
-         int i=0;
-         int m= 0;
-         int d= 0;
-int arr[]= new int[2]; 
-        while(i<nums.length){
-            int correct = nums[i]-1;
-        if(nums[correct] != nums[i]){
-            int temp = nums[i];
-            nums[i]= nums[correct];
-            nums[correct]= temp;
-        }
-        else{
-            i++;
-        }
-    }
-    for(int j=0;j<nums.length;j++){
-        if(nums[j]!=j+1){
-      return new int[] {nums[j],j+1};
-        }
-    }
-    return new int[] {-1,-1};
-    
+    public int[] findErrorNums(int[] nums) {
+      int arr[]= new int[nums.length+1];
+      int dup=-1, mis=-1;
+      for(int n:nums){
+        arr[n]++;
+      }
+      for(int i=1;i<=nums.length;i++){
+        if(arr[i]==0) mis=i;
+        if(arr[i]>1) dup=i;
+      }
+
+      return new int[]{dup,mis};  
     }
 }
