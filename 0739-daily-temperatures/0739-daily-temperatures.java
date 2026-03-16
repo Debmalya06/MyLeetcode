85bf379c7b941dcdd27b1c680f8156fc526128f9
Debmalya Pan <134578586+Debmalya06@users.noreply.github.com>
2026-03-16 09:35:39 +0530

    Time: 61 ms (67.93%), Space: 107.7 MB (43.62%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
         Stack<Integer>stack1 = new Stack<>();
-        Stack<Integer>stack2= new Stack<>();
-        HashMap<Integer, Integer> map =new HashMap<>();
+        int ans[] = new int [temp.length];
+        
 
         for(int i=0;i<temp.length;i++){
-            while(!stack1.isEmpty() && stack1.peek()<temp[i]){
-                int x= stack2.pop();
-                map.put(x,i-x);
-                stack1.pop();
+            while(!stack1.isEmpty() && temp[stack1.peek()]<temp[i]){
+               int x= stack1.pop();
+                ans[x]=i-x;
             }
-            stack1.push(temp[i]);
-            stack2.push(i);
+            stack1.push(i);
         }
-        int ans[] = new int[temp.length];
-
-        for(int i=0;i<temp.length;i++){
-            ans[i] = map.getOrDefault(i, 0);
-        }
+       
         return ans;
 
     }
