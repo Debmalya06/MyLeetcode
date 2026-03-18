9cae54607d7ccf49ed2b07441ae4ba7664e9f9e8
Debmalya Pan <134578586+Debmalya06@users.noreply.github.com>
2026-03-18 10:26:38 +0530

    Time: 5 ms (66.42%), Space: 46.9 MB (67.57%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int n : asteroids) {
+
+            while (!stack.isEmpty() && stack.peek() > 0 && n < 0) {
+
+                if (stack.peek() < -n) {
+                    stack.pop();
+                    continue;
+                }
+                else if (stack.peek() == -n) {
+                    stack.pop();
+                }
+
+                n = 0;// to donot push further
+                break;
+            }
+
+            if (n != 0) {
+                stack.push(n);
+            }
+        }
+
+        int[] ans = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            ans[i] = stack.pop();
+        }
+        return  ans;
+    }
+}
