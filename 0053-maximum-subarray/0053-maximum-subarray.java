574aa14a7d2e27674e67def22362a08c10859f91
Debmalya Pan <134578586+Debmalya06@users.noreply.github.com>
2026-03-29 13:48:11 +0530

    Time: 1 ms (99.95%), Space: 77.5 MB (26.67%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,15 +1,14 @@
 class Solution {
-    public int maxSubArray(int[] nums) {
+    public int maxSubArray(int[] arr) {
     //as problem array have atleast one element then not need to think about empty array
     //[1,-2,3]--> default first ans =1and bestend =1-->
-       int bestend= nums[0];
-       int ans = nums[0];
-      for(int i=1;i<nums.length;i++){
-        int a= nums[i]+bestend;
-        int b=nums[i];
-        bestend = Math.max(a,b);
-        ans = Math.max(bestend,ans);
-      }
-        return ans; 
+     int best=arr[0];
+    int ans = arr[0];
+    for(int i=1;i<arr.length;i++){
+        int curr = arr[i];
+        best = Math.max(curr, curr+best);
+        ans = Math.max(best,ans);
+    }
+    return ans;
     }
 }
